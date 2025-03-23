af81f978fac2f1e7a8884ab3d01823daa1f9cf8f
yangkun <1276406243@qq.com>
2025-03-23 10:46:59 +0800

    Exercise10

--- src/StoreToRent.java
+++ src/StoreToRent.java
@@ -14,6 +14,40 @@ public class StoreToRent {
     private String minimumLeasePeriod;
     private String floorNumber;
     private boolean available;
+    
+    private final double INTEREST_RATE = 0.25;
+    private boolean loanRequired;
+    private double loanAmount;
+    private int loanPaymentTerm;
+
+    public double getINTEREST_RATE() {
+        return INTEREST_RATE;
+    }
+
+    public boolean isLoanRequired() {
+        return loanRequired;
+    }
+
+    public double getLoanAmount() {
+        return loanAmount;
+    }
+
+    public int getLoanPaymentTerm() {
+        return loanPaymentTerm;
+    }
+
+    public StoreToRent(boolean loanRequired, double loanAmount, int loanPaymentTerm) {
+        this.loanRequired = loanRequired;
+        this.loanAmount = loanAmount;
+        this.loanPaymentTerm = loanPaymentTerm;
+    }
+
+    public double calculateLoanFinancing() {
+        if (loanRequired) {
+            return (loanAmount * (1 + INTEREST_RATE)) / loanPaymentTerm;
+        }
+        return 0;
+    }
 
     public String getStoreName() {
         return storeName;
